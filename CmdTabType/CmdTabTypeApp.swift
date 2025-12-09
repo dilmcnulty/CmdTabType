48c560d0e1812a10db2342632613dbe8852b16c0
dilmcnulty <dmcnulty97@gmail.com>
2025-12-09 16:12:22 -0600

    Initial Commit

---
+++ CmdTabType/CmdTabTypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CmdTabTypeApp.swift
+//  CmdTabType
+//
+//  Created by Dillon McNulty on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CmdTabTypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
